9137f9b4f51fdb4664968c665217586e9a41db3c
Ku-Al <kubov.aleksandr@gmail.com>
2014-07-03 19:06:36 +0000

    modified:   dev/django.ini modified:   dev/photoplanet

--- photoplanet/photoplanet/wsgi.py
+++ photoplanet/photoplanet/wsgi.py
@@ -27,6 +27,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "photoplanet.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
+
+
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
